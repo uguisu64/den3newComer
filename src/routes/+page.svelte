3be440a90ccaa2206bd83c690b671fc64780cfe0
uguisu64 <soma64soma@gmail.com>
2025-03-26 21:08:18 +0900

    fix font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,28 +5,37 @@
 
 <header>
     <h1>電子計算機研究会 新歓ページ</h1>
-    <p>芝浦工業大学公認サークル</p>
-    </header>
-    
-    <div class="container">
-    
-        <section>
-            <h2>電子計算機研究会とは</h2>
-            <p>情報系の技術に興味を持った芝浦工業大学の学生が運営する大学公認サークルです．</p>
-            <p><a href="https://x.com/sit_densan" target="_blank">▶︎ 電算公式Twitterアカウント</a></p>
-        </section>
-    
-        <section>
-            <ZoomMeeting></ZoomMeeting>
-        </section>
-    
-        <section>
-            <Activity></Activity>
-        </section>
-    
-    </div>
+<p>芝浦工業大学公認サークル</p>
+</header>
 
-  <style>
+<div class="container">
+
+    <section>
+        <h2>電子計算機研究会とは</h2>
+        <p>情報系の技術に興味を持った芝浦工業大学の学生が運営する大学公認サークルです．</p>
+        <p><a href="https://x.com/sit_densan" target="_blank">▶︎ 電算公式Twitterアカウント</a></p>
+    </section>
+
+    <section>
+        <ZoomMeeting></ZoomMeeting>
+    </section>
+
+    <section>
+        <Activity></Activity>
+    </section>
+
+</div>
+
+<svelte:head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&display=swap');
+        body {
+            font-family: "Noto Sans JP", sans-serif;
+        }
+    </style>
+</svelte:head>
+
+<style>
     header {
         background-color: #007BFF;
         padding: 2rem;
@@ -37,6 +46,7 @@
     h1 {
         margin-bottom: 0.5rem;
         font-size: 2rem;
+        
     }
 
     .container {
@@ -69,4 +79,4 @@
         padding-left: 0.5rem;
         margin-bottom: 1rem;
     }
-  </style>
+</style>
